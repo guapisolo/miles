ce7f92308499664e52b4c986d0eb57c622b720db
fzyzcjy <ch271828n@outlook.com>
2026-01-14 22:55:32 +0800

    fmt

--- miles/rollout/modular_rollout/orchestration_common.py
+++ miles/rollout/modular_rollout/orchestration_common.py
@@ -7,7 +7,7 @@ from typing import Any
 from miles.rollout.base_types import GenerateFnInput
 from miles.rollout.modular_rollout.compatibility import load_generate_function
 from miles.rollout.modular_rollout.inference_wrapper import generate
-from miles.rollout.rm_hub import async_rm, batched_async_rm
+from miles.rollout.rm_hub import batched_async_rm
 from miles.utils.misc import listify
 from miles.utils.processing_utils import load_processor, load_tokenizer
 from miles.utils.types import Sample
