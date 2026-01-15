6f06f0f15c5136e2facc87478cc0c993abf86964
fzyzcjy <ch271828n@outlook.com>
2026-01-15 09:35:33 +0800

    more

--- tests/fixtures/rollout_integration.py
+++ tests/fixtures/rollout_integration.py
@@ -9,7 +9,7 @@ from unittest.mock import patch
 import pytest
 import requests
 
-from miles.rollout.data_source import RolloutDataSourceWithBuffer
+from miles.rollout.data_source import RolloutDataSourceWithBuffer, DataSource
 from miles.rollout.modular_rollout.orchestration_common import GenerateState
 from miles.router.router import MilesRouter
 from miles.utils.arguments import parse_args
@@ -29,7 +29,7 @@ class IntegrationEnvConfig:
 @dataclass(frozen=True)
 class IntegrationEnv:
     args: Namespace
-    data_source: "RolloutDataSourceWithBuffer"
+    data_source: DataSource
     mock_server: MockSGLangServer
 
 
