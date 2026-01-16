2b207a3a36b90f58bd916560319e6145360409e3
fzyzcjy <ch271828n@outlook.com>
2026-01-16 09:17:36 +0800

    more

--- tests/utils/test_utils/test_mock_tools.py
+++ tests/utils/test_utils/test_mock_tools.py
@@ -1,14 +1,11 @@
 from miles.utils.test_utils.mock_tools import execute_tool_call
 
-YEAR = 2026
-TEMPERATURE = -60
-
 
 class TestExecuteToolCall:
     def test_execute_get_year(self):
         result = execute_tool_call("get_year", {})
-        assert result == {"year": YEAR}
+        assert result == {"year": 2026}
 
     def test_execute_get_temperature(self):
         result = execute_tool_call("get_temperature", {"location": "Mars"})
-        assert result == {"temperature": TEMPERATURE}
+        assert result == {"temperature": -60}
