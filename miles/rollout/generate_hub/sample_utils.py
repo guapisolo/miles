c27e690c44909f9478dc99f5bbd28e09a5de6d60
fzyzcjy <ch271828n@outlook.com>
2026-01-17 13:27:57 +0800

    more

--- miles/rollout/generate_hub/sample_utils.py
+++ miles/rollout/generate_hub/sample_utils.py
@@ -37,7 +37,7 @@ def merge_samples(a: Sample, b: Sample, tokenizer) -> Sample:
         response_length=a.response_length + obs_len + b.response_length,
         label=_merge_equal_value("label"),
         reward=_merge_equal_value("reward"),
-        loss_mask=a_loss_mask + [0] * obs_len + b_loss_mask,
-        rollout_log_probs=a_log_probs + [0.0] * obs_len + b_log_probs,
+        loss_mask=a.loss_mask + [0] * obs_len + b.loss_mask,
+        rollout_log_probs=a.rollout_log_probs + [0.0] * obs_len + b.rollout_log_probs,
         status=b.status,
     )
